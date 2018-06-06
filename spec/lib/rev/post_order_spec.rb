29df536bf03f7bb5aa68942e9b80d612e4a8f075
Blake Henry <blake@rev.com>
2018-06-06 14:13:37 -0500

    Bump minor version number. Add tests for json formatting

--- spec/lib/rev/post_order_spec.rb
+++ spec/lib/rev/post_order_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../../spec_helper'
+require 'pry'
 
 describe 'POST /orders' do
   let(:client) { Rev.new('welcome', 'AAAAAu/YjZ3phXU5FsF35yIcgiA=', 'www.revtrunk.com') }
@@ -16,12 +17,12 @@ describe 'POST /orders' do
   let(:balance_payment) { Rev::Payment.new(Rev::Payment::TYPES[:account_balance]) }
   let(:transcription_inputs) {
       inputs = []
-      inputs << Rev::Input.new(:external_link => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc')
-      inputs << Rev::Input.new(:audio_length_seconds => 900, :external_link => 'https://vimeo.com/7976699')
+      inputs << Rev::Input.new(:external_link => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc', accents: ['AmericanNeutral', 'Australian'])
+      inputs << Rev::Input.new(:audio_length_seconds => 900, :external_link => 'https://vimeo.com/7976699', speaker_names: ['Billy', 'Bob'], glossary: ['Sandwich'])
   }
   let(:caption_inputs) {
     inputs = []
-    inputs << Rev::Input.new(:video_length_seconds => 900, :external_link => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc')
+    inputs << Rev::Input.new(:video_length_seconds => 900, :external_link => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc', speaker_names: ['Billy', 'Bob'], glossary: ['Sandwich'])
   }
   let(:transcription_options) { Rev::TranscriptionOptions.new(transcription_inputs,
     :verbatim => true, :timestamps => true) }
@@ -50,8 +51,8 @@ describe 'POST /orders' do
       'non_standard_tat_guarantee' => false,
       'transcription_options' => {
         'inputs' => [
-          { 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc' },
-          { 'external_link' => 'https://vimeo.com/7976699', 'audio_length_seconds' => 900 }
+          { 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc', 'accents' => ['AmericanNeutral', 'Australian']},
+          { 'external_link' => 'https://vimeo.com/7976699', 'audio_length_seconds' => 900, 'speaker_names' => ['Billy', 'Bob'], 'glossary' => ['Sandwich'] }
         ],
         'verbatim' => true,
         'timestamps' => true
@@ -77,8 +78,8 @@ describe 'POST /orders' do
         'non_standard_tat_guarantee' => false,
         'transcription_options' => {
             'inputs' => [
-                { 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc' },
-                { 'external_link' => 'https://vimeo.com/7976699', 'audio_length_seconds' => 900 }
+                { 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc', 'accents' => ['AmericanNeutral', 'Australian']},
+                { 'external_link' => 'https://vimeo.com/7976699', 'audio_length_seconds' => 900, 'speaker_names' => ['Billy', 'Bob'], 'glossary' => ['Sandwich'] }
             ],
             'verbatim' => true,
             'timestamps' => true
@@ -114,7 +115,7 @@ describe 'POST /orders' do
       'non_standard_tat_guarantee' => false,
       'caption_options' => {
         'inputs'=> [
-          { 'video_length_seconds' => 900, 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc' }
+          { 'video_length_seconds' => 900, 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc', 'speaker_names' => ['Billy', 'Bob'], 'glossary' => ['Sandwich'] }
         ],
         'output_file_formats' => [Rev::CaptionOptions::OUTPUT_FILE_FORMATS[:subrip]]
       }
@@ -137,7 +138,7 @@ describe 'POST /orders' do
       'non_standard_tat_guarantee' => false,
       'caption_options' => {
         'inputs'=> [
-          { 'video_length_seconds' => 900, 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc' }
+          { 'video_length_seconds' => 900, 'external_link' => 'http://www.youtube.com/watch?v=UF8uR6Z6KLc', 'speaker_names' => ['Billy', 'Bob'], 'glossary' => ['Sandwich'] }
         ],
         'subtitle_languages' => ['es','it'],
         'output_file_formats' => [Rev::CaptionOptions::OUTPUT_FILE_FORMATS[:subrip]]
